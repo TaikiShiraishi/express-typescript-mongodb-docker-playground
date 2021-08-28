e17398af40aaf2f287badc5494a1d6075a1dd70c
TaikiShiraishi <operatic-earthquake+github@pm.me>
2021-08-28 23:18:03 +0900

    feat: add image_path in Message Model

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -4,12 +4,14 @@ interface Message {
   username: string
   message: string
   date: Date
+  image_path: string
 }
 
 const MessageSchema = new Schema<Message>({
   username: String,
   message: String,
   date: { type: Date, default: new Date() },
+  image_path: String
 })
 
 export const MessageModel = model<Message>('Message', MessageSchema)
