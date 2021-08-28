1fac6bb923674278594ee64a2cf655fb0aef2a0d
TaikiShiraishi <operatic-earthquake+github@pm.me>
2021-08-28 23:25:15 +0900

    feat: 画像投稿作成

--- src/routes/updateRoutes.ts
+++ src/routes/updateRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { MessageModel } from '../models/Message'
+import { UploadedFile } from 'express-fileupload'
 
 const router = express.Router()
 
@@ -22,7 +23,7 @@ router.get('/', function (req, res, next) {
  *   post:
  *    description: message を create する
  *    produces:
- *      - application/json
+ *      - multipart/form-data
  *    parameters:
  *      - name: username
  *        in: formData
@@ -34,19 +35,40 @@ router.get('/', function (req, res, next) {
  *        required: true
  *        type: string
  *        description: メッセージ
+ *      - name: image
+ *        in: formData
+ *        type: image/png
+ *        format: binary
+ *        description: 画像
  *    response:
  *      200:
  *        description: 成功
  */
 router.post('/', function (req, res, next) {
-  const newMessage = new MessageModel({
-    username: req.body.username,
-    message: req.body.message,
-  })
-  newMessage.save((err) => {
-    if (err) throw err
-    return res.redirect('/')
-  })
+  if (req.files && req.files.image) {
+    const image = req.files?.image as UploadedFile
+    image.mv(`./public/images/update/${image.name}`, function (err) {
+      if (err) throw err
+      const newMessage = new MessageModel({
+        username: req.body.username,
+        message: req.body.message,
+        image_path: `/images/update/${image.name}`,
+      })
+      newMessage.save((err) => {
+        if (err) throw err
+        return res.redirect('/')
+      })
+    })
+  } else {
+    const newMessage = new MessageModel({
+      username: req.body.username,
+      message: req.body.message,
+    })
+    newMessage.save((err) => {
+      if (err) throw err
+      return res.redirect('/')
+    })
+  }
 })
 
 export default router
